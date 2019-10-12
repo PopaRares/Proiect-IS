4434cb903592ad96c36a9444c44ccca54387dbe9
arrays_start_at_0 <raresp98@gmail.com>
2019-10-12 18:54:11 +0300

    Boiler Plate REST

---
+++ src/main/java/com/IS/SINU/Resource/Test.java
@@ -0,0 +1,10 @@
+package com.IS.SINU.Resource;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+public @Data @AllArgsConstructor
+class Test {
+    private String name;
+    private int grade;
+}
---
+++ src/main/java/com/IS/SINU/controllers/TestController.java
@@ -0,0 +1,16 @@
+package com.IS.SINU.controllers;
+
+import com.IS.SINU.Resource.Test;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/test")
+    public Test test(@RequestParam(value="name", defaultValue="World") String name, @RequestParam(value="grade", defaultValue="5") int grade) {
+        return new Test(name, grade);
+    }
+
+}
