03ec80a0795a946ab17cab63631baf018cbbe1bc
Rares Popa <raresp98@gmail.com>
2020-01-04 20:10:42 +0200

    Created GradeRepository.

---
+++ src/main/java/com/IS/SINU/repositories/GradeRepository.java
@@ -0,0 +1,17 @@
+package com.IS.SINU.repositories;
+
+import com.IS.SINU.entities.dao.Grade;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface GradeRepository extends JpaRepository<Grade, Long> {
+
+    @Query("from Grade G where G.student.username = ?1")
+    Grade findByUsername(String username);
+
+    @Query("from Grade G where G.student.username = ?1 and G.teaching.class_name.subject.name = ?2")
+    Grade findBySubject(String username, String subjectName);
+
+    @Query("from Grade G where G.student.username = ?1 and G.teaching.class_name.subject.name = ?2 and G.teaching.class_name.type = ?3")
+    Grade findByClassType(String username, String subjectName, String classType);
+}
